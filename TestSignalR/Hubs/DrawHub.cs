cd7184d7469b9a6ff8c6673e7f41439dd8dd42da
the-shichko <pashashichko0075@gmail.com>
2021-02-02 11:51:52 +0300

    [add] files

---
+++ TestSignalR/Hubs/DrawHub.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TestSignalR.Hubs
+{
+    public class DrawHub : Hub
+    {
+        public Task Draw(int prevX, int prevY, int currentX, int currentY)
+        {
+            return Clients.All
+                .SendAsync("DrawLine", prevX, prevY, currentX, currentY);
+        }
+    }
+}
---
+++ TestSignalR/wwwroot/js/draw.js
@@ -0,0 +1,42 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/drawhub").build();
+connection.start();
+
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+const myPics = document.getElementById('myPics');
+const context = myPics.getContext('2d');
+connection.on("DrawLine", function (x1, y1, x2, y2) {
+    console.log("ok");
+    context.beginPath();
+    context.strokeStyle = 'black';
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+});
+
+myPics.addEventListener('mousedown', e => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+
+myPics.addEventListener('mousemove', e => {
+    if (isDrawing === true) {
+        connection.invoke("Draw", x, y, e.offsetX, e.offsetY)
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+window.addEventListener('mouseup', e => {
+    if (isDrawing === true) {
+        connection.invoke("Draw", x, y, e.offsetX, e.offsetY)
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+});
